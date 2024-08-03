990ab99ca9bfbc2dd5b415e154b6884bc4494b62
Umme-Fathima-S <kubrafathima092@gmail.com>
2024-08-03 22:03:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_route_table" "public_route_table" {
     gateway_id  = "aws_internet_gateway.igw.id"
   }
 }
-/* ROUT TABLE ASSOCIATION */
+/* ROUTE TABLE ASSOCIATION */
 resource "aws_route_table_association" "public_route_table_association" {
   subnet_id      = "aws_subnet.public_subnet.id"
   route_table_id = "aws_route_table.public_route_table.id"
@@ -57,12 +57,12 @@ resource "aws_route_table_association" "private_route_table_association" {
 /* AWS KMS KEY */
 resource "aws-kms_key" "CMK" {
   key_name   = "aws_key"
-  public_key = "ssh-rsa.....
+  public_key = "ssh-rsa1234abcd-12ab-34cd-56ef-1234567890ab"
 }
 data "aws_availability_zones" "available_zones" {
 /* EC2 INSTANCE IN PRIVATE SUBNET */
 resource "aws_instance" "ec2_instance" {
-  ami               = "ami...
+  ami               = "ami0767046d1677be5a0"
   instance_type     = "t2.micro"
   availability_zone = "data.aws_availability_zones.available_zones.names[0]"
   key_name          = "aws_key"
@@ -71,11 +71,11 @@ resource "aws_instance" "ec2_instance" {
 /* AWS RDS INSTANCE */
 resource "aws_db_instance" "db_instance" {
   engine            = "mysql"
-  allocated storage = 10
+  allocated storage =  10
   instance_class    = "db.t2.micro"
   availability_zone = "data.aws_availability_zones.available_zones.names[0]"
-  username          = "
-  password          = "
+  username          = "rdsusername"
+  password          = "rdspassword"
   subnet_id         = "aws_subnet.private_subnet.id"
 }
 
